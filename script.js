36d009348402896a11da1c9d68ab4cfcdc0c34ba
506-4 <506-4@BELDORNII.LOCAL>
2022-11-16 12:44:30 +0300

    vowels

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ String.prototype.toAlternatingCase = function () {
   
 
   //__________________________________________________________end/
+
+
+  //task 2________________________________________________
+
+//   Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+
+function disemvowel(str) {
+  let consonants = str.replace(/[aeiou]/gi, '');
+  return consonants;
+}
+
+//end___________________________________
