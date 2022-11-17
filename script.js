7cced1eefc4790011e6bbfe4f45b28d103af1774
Ariiychyk Denis <Areychyk@mail.ru>
2022-11-17 21:28:54 +0300

    task 10

--- script.js
+++ script.js
@@ -176,3 +176,38 @@
 //   }
 //   return sum
 // }
+
+
+//end___________________________________
+
+
+ //task 10________________________________________________
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+
+// function getSum( a,b ){
+//   let max = Math.max(a,b);
+//    let min = Math.min(a,b);
+//   let sum = max;
+//   for (let i=min; i<max; i++)
+//    sum= sum +i;
+//   return sum
+// }
+
+
+
+//end___________________________________
+
+
+ //task 11________________________________________________
