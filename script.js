844c40cb7509cfb0c74e211067fe56f44f651941
Ariiychyk Denis <areychyk@mail.ru>
2022-12-08 22:06:52 +0300

    task 14

--- script.js
+++ script.js
@@ -240,7 +240,6 @@
 //task 12___________________________________________
 
 
-
 // A square of squares
 // You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 //
@@ -289,3 +288,41 @@
 // // console.log(Math.sqrt(26) % 2)
 // console.log(square(-1))
 // console.log(Number.isInteger())
+
+
+// __________end_____________________________
+
+
+// ________________task 13___________________________
+
+// Your task is to create a function that does four basic mathematical operations.
+//
+//     The function should take three arguments - operation(string/char), value1(number), value2(number).
+//     The function should return result of numbers after applying the chosen operation.
+//
+// Examples(Operator, value1, value2) --> output
+
+//
+// function basicOp(operation, value1, value2) {
+//     if (operation === "+") {
+//         return value1 + value2
+//     }
+//     if (operation === "-") {
+//         return value1 - value2
+//     }
+//     if (operation === "*") {
+//         return value1 * value2
+//     }
+//     if (operation === "/") {
+//         return value1 / value2
+//     }
+//
+// }
+// // ('+', 4, 7) --> 11
+// // ('-', 15, 18) --> -3
+// // ('*', 5, 5) --> 25
+// // ('/', 49, 7) --> 7
+// console.log(basicOp('+', 4, 7))
+// console.log(basicOp('-', 15, 18))
+// console.log(basicOp('*', 5, 5))
+// console.log(basicOp('/', 49, 7))
