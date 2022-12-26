2470ed9c3aabc38930d6bba9793c273e6fbf6981
Ariiychyk Denis <areychyk@mail.ru>
2022-12-26 16:06:21 +0300

    task 31

--- script.js
+++ script.js
@@ -751,3 +751,18 @@
 //     }
 //
 // }
+
+
+
+
+//end___________________________________
+
+//task 31_____________________________________
+
+
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+//
+//     function removeExclamationMarks(s) {
+//     return s.replace(/[!]/gi,"")
+// }
