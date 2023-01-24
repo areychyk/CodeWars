07e43396e1d035603d6609be691518dfbfbdecbf
Ariiychyk Denis <areychyk@mail.ru>
2023-01-24 21:39:08 +0300

    task 40

--- script.js
+++ script.js
@@ -1084,3 +1084,53 @@
 // console.log(trim("Code Wars is pretty rad", 50))
 // console.log(trim("iLwT", 3))//
 // console.log(trim("CFfWf", 3))//CFf...
+
+
+
+
+//end___________________________________
+
+//task 40_____________________________________
+
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+//
+//     It should remove all values from list a, which are present in list b keeping their order.
+//
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+//
+//     arrayDiff([1,2,2,2,3],[2]) == [1,3]
+//
+//
+//
+
+// function arrayDiff(a, b) {
+//     let result =[]
+//      for (let i = 0; i < a.length; i++) {
+//         if(!b.includes(a[i])){
+//             result.push(a[i])
+//         }
+//     }
+//
+//
+//     return result
+//
+// }
+//
+//
+// // console.log(arrayDiff([1,2], [1]))
+// // console.log(arrayDiff([1,2,2], [1]))
+// console.log(arrayDiff([], [1,2]))//
+// console.log(arrayDiff([1,2,2], []))
+// console.log(arrayDiff([1,2,3], [1,2]))
+
+
+
+
+// assert.deepEqual(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+// assert.deepEqual(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+// assert.deepEqual(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+// assert.deepEqual(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+// assert.deepEqual(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
+// assert.deepEqual(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
