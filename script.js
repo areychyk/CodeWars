6e35638cafed4f85fadd2a079d02a121a32bd583
Ariiychyk Denis <areychyk@mail.ru>
2023-03-23 20:55:54 +0300

    task 47

--- script.js
+++ script.js
@@ -1332,17 +1332,41 @@
 
 
 
-function narcissistic(value) {
-    // Code me to return true or false
-    let arr=value.toString().split('')
-    let square= arr.map((num,index)=>num**arr.length)
-    let result=0;
-    for (let i = 0; i < square.length; i++) {
-        result += square[i]
-    }
-    return result===value
-}
+//
 
 
-console.log(narcissistic(153))
-console.log(narcissistic(1652 ))
+
+//end___________________________________
+
+
+//task 47_____________________________________
+
+
+// The male gametes or sperm cells in humans and other mammals are heterogametic and contain one of two types of sex chromosomes. They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homogametic.
+//
+//     The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
+//
+//     Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+//
+// If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+//
+
+// function chromosomeCheck(sperm) {
+//     let result = sperm.split('').filter(s=>s==='X')
+//     console.log(result)
+//     if(result.length>1){
+//         return `Congratulations! You're going to have a daughter.`
+//     }else{
+//         return `Congratulations! You're going to have a son.`
+//     }
+// }
+//
+//
+// console.log(chromosomeCheck('XX'))
+// console.log(chromosomeCheck('XY'))
+
+
+
+
+// Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.")
+// Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.")
